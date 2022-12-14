f71df76ad6248fda3161841c74dd12b2f5406e58
DESKTOP-RUL295T\adity <adityafjri@gmail.com>
2022-12-14 21:10:35 +0700

    fitur pencarian

--- assets/js/cosmetic.js
+++ assets/js/cosmetic.js
@@ -1,3 +1,22 @@
+//search button
+
+$(document).ready(function(){
+	$('a[href="#search"]').on('click', function(event) {                    
+		$('#search').addClass('open');
+    $('#search-button').addClass('open');
+		$('#search > form > input[type="search"]').focus();
+	});            
+	$('#search, #search button.close').on('click keyup', function(event) {
+		if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+			$(this).removeClass('open');
+      $('#search-button').removeClass('open');
+		}
+	});            
+});
+
+
+//slider home
+
 var $slider = $('.slideshow .slider'),
   maxItems = $('.item', $slider).length,
   dragging = false,

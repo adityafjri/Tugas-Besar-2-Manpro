63cc39ba378fde8e664849183ebe0e69f1bb250d
Fabrianus <6181901075@student.unpar.ac.id>
2022-12-14 20:33:44 +0700

    mencoba menghubungkan ke db

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mysql from 'mysql'
+import path from 'path';
+
+const port = 8080;
+const app = express();
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.resolve('public')));
+
+app.get('/',async(req,res)=>{
+    res.render('home')
+});
+
+app.get('/pencarian.ejs',async(req,res)=>{
+    res.render('pencarian')
+})
+app.get('/grafik.ejs',async(req,res)=>{
+    res.render('grafik')
+})
+app.get('/graph.ejs',async(req,res)=>{
+    res.render('graph')
+})
+app.listen(port,()=>{
+    console.log('ready!');
+});
